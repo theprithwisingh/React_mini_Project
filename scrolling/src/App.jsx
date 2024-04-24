db8ff48abb579b2fcff01a4e466c491c052a8227
theprithwisingh <prithwisingh77@gmail.com>
2024-04-24 21:24:22 +0530

    second

--- scrolling/src/App.jsx
+++ scrolling/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+// import Feedback from './Components/Feedback.jsx'
+import Feedback from './Components/Feedback';
 function App() {
   return (
     <>
-      
+      <div className='heading1'>
+        <h1>Hello world</h1>
+        <h2>Im Prithwi singh</h2>
+      </div>
+      <Feedback/>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ scrolling/src/Components/Feedback.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Feedback.css'
+function Feedback() {
+  return (
+    <div className='feedback'>
+      <div className='feed'>
+        <div className='name_and_photo'>
+          <img src="" alt="" />
+          <p></p>
+          <p></p>
+        </div>
+        <div className='feedback_content'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam consectetur nihil vero minima accusamus! Ut doloremque nulla cupiditate accusantium!
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Feedback;
