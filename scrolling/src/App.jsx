93e88cc9509fe7291da34ebae86e76beaa4a46bc
theprithwisingh <prithwisingh77@gmail.com>
2024-04-26 03:35:19 +0530

    third

--- scrolling/src/App.jsx
+++ scrolling/src/App.jsx
@@ -3,13 +3,13 @@ import './App.css'
 import Feedback from './Components/Feedback';
 function App() {
   return (
-    <>
+    <div className='App'>
       <div className='heading1'>
         <h1>Hello world</h1>
         <h2>Im Prithwi singh</h2>
       </div>
       <Feedback/>
-    </>
+    </div>
   )
 }
 
--- scrolling/src/Components/Feedback.jsx
+++ scrolling/src/Components/Feedback.jsx
@@ -1,19 +1,74 @@
 import React from 'react'
+import photo from '../assets/photo.jpeg'
 import './Feedback.css'
 function Feedback() {
   return (
-    <div className='feedback'>
+  <div className='feedback'>
+    {/*1*/}
       <div className='feed'>
         <div className='name_and_photo'>
-          <img src="" alt="" />
-          <p></p>
-          <p></p>
+          <img src={photo} alt="" />
+          <p>Prithwi Singh</p>
         </div>
         <div className='feedback_content'>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam consectetur nihil vero minima accusamus! Ut doloremque nulla cupiditate accusantium!
         </div>
       </div>
-    </div>
+
+      {/*2*/}
+      <div className='feed'>
+        <div className='name_and_photo'>
+          <img src={photo} alt="" />
+          <p>Prithwi Singh</p>
+        </div>
+        <div className='feedback_content'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam consectetur nihil vero minima accusamus! Ut doloremque nulla cupiditate accusantium!
+        </div>
+      </div>
+
+      {/*3*/}
+      <div className='feed'>
+        <div className='name_and_photo'>
+          <img src={photo} alt="" />
+          <p>Prithwi Singh</p>
+        </div>
+        <div className='feedback_content'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam consectetur nihil vero minima accusamus! Ut doloremque nulla cupiditate accusantium!
+        </div>
+      </div>
+      {/*4*/}
+      <div className='feed'>
+        <div className='name_and_photo'>
+          <img src={photo} alt="" />
+          <p>Prithwi Singh</p>
+        </div>
+        <div className='feedback_content'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam consectetur nihil vero minima accusamus! Ut doloremque nulla cupiditate accusantium!
+        </div>
+      </div>
+
+      {/*5*/}
+      <div className='feed'>
+        <div className='name_and_photo'>
+          <img src={photo} alt="" />
+          <p>Prithwi Singh</p>
+        </div>
+        <div className='feedback_content'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam consectetur nihil vero minima accusamus! Ut doloremque nulla cupiditate accusantium!
+        </div>
+      </div>
+
+      {/*6*/}
+      <div className='feed'>
+        <div className='name_and_photo'>
+          <img src={photo} alt="" />
+          <p>Prithwi Singh</p>
+        </div>
+        <div className='feedback_content'>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi totam consectetur nihil vero minima accusamus! Ut doloremque nulla cupiditate accusantium!
+        </div>
+      </div>
+  </div>
   )
 }
 
